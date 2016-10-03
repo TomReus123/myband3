8d01561fa4276c3bd59b8bc7602c43788bec16a5
TomReus123 <tomreus112@gmail.com>
2016-10-03 15:07:12 +0200

    les ingrid

---
+++ views/compiled/24e7cbb65a777107f8d697e5e9a99f75880e673e.file.footer.tpl.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2016-09-28 16:16:59
+         compiled from "views\footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2900757ebd0db7b7060-39194055%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '24e7cbb65a777107f8d697e5e9a99f75880e673e' => 
+    array (
+      0 => 'views\\footer.tpl',
+      1 => 1475072013,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2900757ebd0db7b7060-39194055',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_57ebd0db84b6e0_99916388',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_57ebd0db84b6e0_99916388')) {function content_57ebd0db84b6e0_99916388($_smarty_tpl) {?><footer>
+<p>made by me for school</p>
+</footer>
+</body>
+</html><?php }} ?>
